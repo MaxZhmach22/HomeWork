7ddc18e776de74dc0271f69a73b825944cd778ce
[MaxZhmach] <maxzhmach@mail.ru>
2020-12-15 23:12:32 +0300

    Lesson07 recreated

--- HomeWork/HomeWork07/Program.cs
+++ HomeWork/HomeWork07/Program.cs
@@ -1,17 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace HomeWork07
 {
-    class Program
+    static class Program
     {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
         static void Main()
         {
-            Guess_game guess = new Guess_game();
-            guess.InitializeComponent();
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
         }
     }
 }
